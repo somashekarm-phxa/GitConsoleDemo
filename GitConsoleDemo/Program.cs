e73760087bbbfe62747e8b20b3bc1bea2407eafb
Somashekar Muniramappa <65184401+somashekarm-phxa@users.noreply.github.com>
2020-08-21 13:42:03 -0700

    GitHub Changes

--- GitConsoleDemo/Program.cs
+++ GitConsoleDemo/Program.cs
@@ -11,6 +11,7 @@ namespace GitConsoleDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Git");
+            Console.WriteLine("Hello GitHub");
         }
     }
 }
